5819d83a592f4a38458958a935d704700705c474
Yann Fertat <yann.fertat@digora.com>
2018-07-02 14:25:11 +0200

    check fork return code

--- process/zombies.c
+++ process/zombies.c
@@ -7,26 +7,32 @@
 #include <sys/wait.h>
 
 int main() {
-	pid_t pid;
-	int i; 
+  pid_t pid;
+  int i; 
 
-	for (i = 0; i < 5000; i++)  {  
-		pid = fork();
+  for (i = 0; i < 2000; i++)  {  
+    pid = fork();
 
-		if (pid > 0) {
-			// parent process
-			printf("Zombie #%d born\n", i + 1);
+    if (pid == -1) {
+      printf("fork failed (check ulimit?)\n");
+      exit(1);
+    }
 
-			//int child_status;
-			//waitpid(pid, &child_status, 0);
-			// oops we forgot to wait for the pid, so child is now a zombie!
-		} else {
-			// child process
-			exit(0);
-		}  
-	}  
-	// parent will wait
-	sleep(100);
+    if (pid > 0) {
+      // parent process
+      printf("Zombie #%d born\n", i + 1);
 
-	return 0; 
+      //int child_status;
+      //waitpid(pid, &child_status, 0);
+      // oops we forgot to wait for the pid, so child is now a zombie!
+    } else {
+      // child process
+      exit(0);
+    }  
+  }  
+  // parent will wait
+  printf("Done\n");
+  sleep(100);
+
+  return 0; 
 }
