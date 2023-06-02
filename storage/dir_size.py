d1543a3c4851d886191cccf57d0b1e5340437d14
tatref <tatref@users.noreply.github.com>
2023-06-02 13:56:26 +0200

    add dirs_size

---
+++ storage/dir_size.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import datetime
+from pathlib import Path
+
+try:
+    root = Path(sys.argv[1])
+except:
+    print('Usage:\n  ' + sys.argv[0] + ' /path/... [B|k|M|G]')
+    sys.exit(1)
+
+try:
+    units = sys.argv[2]
+    if units.lower() == 'b':
+        units = 1
+    elif units.lower() == 'k':
+        units = 1024
+    elif units.lower() == 'm':
+        units = 1024 * 1024
+    elif units.lower() == 'g':
+        units = 1024 * 1024 * 1024
+except:
+    units = 1024 * 1024
+
+
+
+infos = {}
+for dir, dirs, files in os.walk(root):
+    dir = Path(dir)
+    for filename in files:
+        file = Path(os.path.join(dir, filename))
+        if not os.path.isfile(file):
+            continue
+        stat = os.stat(file)
+        size = stat.st_size
+        timestamp = datetime.datetime.fromtimestamp(stat.st_mtime)
+        date = timestamp.date()
+        node = infos.setdefault(date, {'size': 0, 'dirs': {}})
+        infos[date]['size'] += size
+        parts = file.parent.relative_to(root).parts
+        for part in parts:
+            _ = node['dirs'].setdefault(part, {'size': 0, 'dirs': {}})
+            node['dirs'][part]['size'] += size
+            node = node['dirs'][part]
+
+if len(infos) == 0:
+    print("No files found")
+    sys.exit(0)
+
+dates = sorted([date for date in infos.keys()])
+print(dates[0], '>', dates[-1])
+
+start_date = input('start_date? ')
+start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+end_date = input('end_date? ')
+end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+days = (end_date - start_date).days + 1
+
+
+for day in range(0, days):
+    date = (start_date + datetime.timedelta(days=day)).date()
+    print(date, end=': ')
+    if not date in infos:
+        print('0')
+        continue
+    size = int(infos[date]['size'] / units)
+    print(size)
+
+
+
+date = input('date? ')
+date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+
+node = infos[date]
+currentpath = root
+
+while True:
+    print('')
+    print(date, currentpath)
+    for dir in node['dirs'].keys():
+        size = int(node['dirs'][dir]['size'] / units)
+        print(size, dir)
+    x = input('? ')
+    if x == '':
+        continue
+    else:
+        if not x in node['dirs'].keys():
+            print('not found')
+            print(node['dirs'].keys())
+            continue
+        node = node['dirs'][x]
+        currentpath = currentpath / x
+
